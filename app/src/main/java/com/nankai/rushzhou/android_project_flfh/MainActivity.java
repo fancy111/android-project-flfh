094a4469b04f56877f1863dd4b713071fe74f48c
zhou_chong <rushzhou@163.com>
2016-07-27 22:08:02 +0800

    first edit

---
+++ app/src/main/java/com/nankai/rushzhou/android_project_flfh/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nankai.rushzhou.android_project_flfh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
